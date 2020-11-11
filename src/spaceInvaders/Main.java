70e4105e69659d894123b8eb68b16bc4e562b3b9
Oliver Mohn <s57998@beuth-hochschule.de>
2020-11-11 21:33:01 +0100

    GameView Prototype -> package spaceInvaders

---
+++ src/spaceInvaders/Main.java
@@ -0,0 +1,23 @@
+package spaceInvaders;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("space_invaders.fxml"));
+        primaryStage.setTitle("Space Invaders 2020");
+        primaryStage.setScene(new Scene(root, 900, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
