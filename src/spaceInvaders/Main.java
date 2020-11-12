1711f1dab3707fdfcd31fcb4e714e0765bce17e3
Oliver Mohn <s57998@beuth-hochschule.de>
2020-11-12 20:15:52 +0100

    new fxml prototypes

--- src/spaceInvaders/Main.java
+++ src/spaceInvaders/Main.java
@@ -10,7 +10,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("space_invaders.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("space_invaders.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("GameOver1P.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("GameOver2P.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("Highscore.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("Home_Screen.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("Instructions.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("Register.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("Start_Screen.fxml"));
         primaryStage.setTitle("Space Invaders 2020");
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
